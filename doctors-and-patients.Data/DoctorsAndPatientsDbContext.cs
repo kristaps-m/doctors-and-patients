8f7d6c6230e1f47e43a4c218d40945b440ca59cc
Kristaps-M <kristapsmitins@gmail.com>
2023-05-27 00:48:11 +0300

    create doctors-and-patients.Data. Create DbContext class

---
+++ doctors-and-patients.Data/DoctorsAndPatientsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using doctors_and_patients.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace doctors_and_patients.Data
+{
+    public class DoctorsAndPatientsDbContext : DbContext, IDoctorsAndPatientsDbContext
+    {
+        public DoctorsAndPatientsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<DoctorPatient> DoctorPatients { get; set; }
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+    }
+}
---
+++ doctors-and-patients.Data/IDoctorsAndPatientsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using doctors_and_patients.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace doctors_and_patients.Data
+{
+    public interface IDoctorsAndPatientsDbContext
+    {
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+        DbSet<DoctorPatient> DoctorPatients { get; set; }
+        DbSet<Doctor> Doctors { get; set; }
+        DbSet<Patient> Patients { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
